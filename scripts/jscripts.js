bc1bec23ce76b70762d16d73a0dff22b13db9dc3
Sahil Singh <shilsingh01@yahoo.com>
2020-11-24 17:37:58 -0800

    adding if else statement and for loop

--- scripts/jscripts.js
+++ scripts/jscripts.js
@@ -1,14 +1,28 @@
 const list_of_courses = [
     {code: "ACIT 1620",
-    name: "Introduction to System Administration"},
+    name: "Fundamental Web Techonologies",
+    key: 1620},
     {code: "COMM 1116",
-    name: "Introduction to System Administration"},
+    name: "Business Communication"},
     {code: "ORGB 1110",
-    name: "Introduction to System Administration"},
+    name: "Organizational Behavior"},
 ]
 
 let user_input = prompt("Please enter a 4 digit number: ");
 
 if (user_input == null){
-    user_input = prompt("Please enter a valid input for a 4 digit number")
+    user_input = prompt("Please enter a valid input for a 4 digit number");
+}
+
+var length_of_courses = list_of_courses.length;
+
+for(var i = 0; i < length_of_courses; i++) {
+    initial_boolean = false;
+    course_key_number = list_of_courses[i].key;
+
+    if (user_input === course_key_number) {
+        console.log("Yes I am taking the course: " + list_of_courses[i].code + " " + list_of_courses[i].name);
+    }else {
+        initial_boolean = true;
+    }
 }
