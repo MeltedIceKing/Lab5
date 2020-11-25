bdaffdfa68749c3704fffafeea321b1ab2389223
Sahil Singh <shilsingh01@yahoo.com>
2020-11-24 18:04:31 -0800

    adding some fixes

--- scripts/jscripts.js
+++ scripts/jscripts.js
@@ -1,31 +1,36 @@
 const list_of_courses = [
     {code: "ACIT 1620",
     name: "Fundamental Web Techonologies",
-    key: 1620},
+    class_key: 1620},
+
     {code: "COMM 1116",
     name: "Business Communication",
-    key: 1116},
+    class_key: 1116},
+
     {code: "ORGB 1110",
     name: "Organizational Behavior",
-    key: 1110},
+    class_key: 1110},
 ]
 
-let user_input = prompt("Please enter a 4 digit number: ");
+let user_input = prompt("Please enter a 4 digit number");
 
 if (user_input == null){
+    user_input = prompt("Please enter a valid input for a 4 digit number");
+}else if (user_input == ''){
     user_input = prompt("Please enter a valid input for a 4 digit number");
 }
 
 var length_of_courses = list_of_courses.length;
+console.log(length_of_courses)
 
 for(var i = 0; i < length_of_courses; i++) {
-    initial_boolean = false;
-    course_key_number = list_of_courses[i].key;
+    course_key_number = list_of_courses[i].class_key
+    initial_boolean = false
 
-    if (user_input === course_key_number) {
+    if (user_input == course_key_number) {
         console.log("Yes I am taking the course: " + list_of_courses[i].code + " " + list_of_courses[i].name);
-    }else {
-        initial_boolean = true;
+    }else if (user_input !== course_key_number) {
+        initial_boolean = true
     }
 }
 
@@ -33,8 +38,10 @@ if (initial_boolean === true) {
     list_of_courses.push({
         code: user_input,
         name: null,
-        key: parseInt(user_input)
+        class_key: parseInt(user_input)
     })
     console.log(list_of_courses)
     console.log("Succesfully added new course")
 }
+
+
