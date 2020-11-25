fd8fffe14ee5703199d44cdd8156b5c7c00e05e9
Sahil Singh <shilsingh01@yahoo.com>
2020-11-24 17:25:57 -0800

    adding codes and names

---
+++ scripts/jscripts.js
@@ -0,0 +1,8 @@
+const list_of_courses = [
+    {code: "ACIT 1620",
+    name: "Introduction to System Administration"},
+    {code: "COMM 1116",
+    name: "Introduction to System Administration"},
+    {code: "ORGB 1110",
+    name: "Introduction to System Administration"},
+]
